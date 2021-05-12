80b31c88972f7369bb2e59e1782bfacf136c0a4d
Peter_Fisker <pet5631@gmail.com>
2021-05-12 14:41:18 +0200

    added code for playing with dask on local cluster

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-import numpy as np
+import matplotlib.pyplot as plt
+import time
+from dask.distributed import Client, LocalCluster
+import dask.array as da
 
-A = np.array([
-    [1, 2, 3],
-    [1, 2, 3],
-    [1, 2, 3]
-])
+if __name__ == '__main__':
+    cluster = LocalCluster(
+    n_workers=2,
+    processes=True,
+    threads_per_worker=1
+    )
+    with Client(cluster) as client:
+        start = time.time()
+        x = da.linspace(0, 10000, 100000)
+        y = x * x
+        plt.plot(x, y)
+        end = time.time() - start
+        print('Computation complete! Stopping workers...')
 
-B = np.transpose(A)+ np.ones(3)
-
-C = np.sum(B*A, 1)
-
-print(C)
+    print(f'Execution completed in {end} seconds')
